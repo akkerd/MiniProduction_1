e06ea361a30aebe617b534b48b0dec723d7f54a9
jonas b <jonas.aksel@gmail.com>
2018-09-02 19:58:55 +0200

    project er lagt ind

---
+++ pipelinetest3/Assets/Editor/Pipeline.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+
+
+namespace UnityEditor
+{
+    public class Pipeline 
+    {
+        [MenuItem("Pipeline/Build: Android")]
+
+        public static void BuildAndroid()
+        {
+            var result = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+            {
+                
+                locationPathName = Path.Combine(pathname, filename),
+                scenes = EditorBuildSettings.scenes.Where(n => n.enabled).Select(n => n.path).ToArray(),
+                target = BuildTarget.Android
+            });
+            Debug.Log(result);
+        }
+
+        public static string pathname
+        {
+            
+
+            get
+            {
+                return (Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "builds"));
+            }
+        }
+
+        public static string filename
+        {
+
+            get
+            {
+                return (DateTime.Now.ToString("yyyyMMddHHmm") + ".apk");
+            }
+        }
+
+    }
+}
