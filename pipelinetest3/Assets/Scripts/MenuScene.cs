2e1f4b430992ce8b99d7546ade8fb56e2cfd5a9d
axksel <jonas.aksel@gmail.com>
2018-09-03 14:52:14 +0200

    menuscene

---
+++ pipelinetest3/Assets/Scripts/MenuScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour {
+
+	public void OnPlayClick()
+    {
+        Debug.Log("Pushed button");
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitClick()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
