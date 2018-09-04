3e8b07176693bded69b1f90112e574796dcf59c6
Carl-Johan Linell <CarlJohanLinell@gmail.com>
2018-09-04 13:48:01 +0200

    MainMenu
    MainMenu Added with splash screens

---
+++ MiniProduction_1/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour {
+    bool settingsVisible = false;
+
+    public GameObject settingsMenu;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+    public Toggle musicToggle;
+    public Toggle sfxToggle;
+
+    public Color chosen;
+    public Color unChosen;
+    public Image english;
+    public Image danish;
+
+    public void ChangeSettingsMenu()
+    {
+        if (settingsVisible)
+        {
+            settingsVisible = false;
+            settingsMenu.SetActive(false);
+        } else
+        {
+
+            settingsVisible = true;
+            settingsMenu.SetActive(true);
+        }
+        SetupFlags();
+    }
+
+    public void ChangeMusicValue()
+    {
+        SettingsFlags.Instance.MusicVolume = (int)musicVolumeSlider.value;
+    }
+    public void SwithMusic()
+    {
+        SettingsFlags.Instance.IsMusicOn = musicToggle.isOn;
+    }
+    public void ChangeSFXValue()
+    {
+        SettingsFlags.Instance.SFXVolume = (int)sfxVolumeSlider.value;
+
+    }
+    public void SwithSFX()
+    {
+        SettingsFlags.Instance.IsMusicOn = sfxToggle.isOn;
+    }
+
+    public void ChangeLanguage(int value)
+    {
+        SettingsFlags.Instance.CurrentLanguage = (SettingsFlags.Language)value;
+        SetupFlags();
+    }
+
+    void SetupFlags()
+    {
+        if (SettingsFlags.Instance.CurrentLanguage == 0)
+        {
+            english.color = chosen;
+            danish.color = unChosen;
+        } else
+        {
+            english.color = unChosen;
+            danish.color = chosen;
+        }
+    }
+}
