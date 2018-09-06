c4c19579cdb737c2cf2a2f3e3e78e046bd138907
jonas <murraysomerville@hotmail.com>
2018-09-06 15:31:52 +0200

    change pipeline

--- MiniProduction_1/Assets/Editor/Pipeline.cs
+++ MiniProduction_1/Assets/Editor/Pipeline.cs
@@ -13,6 +13,15 @@ namespace UnityEditor
 
         public static void BuildAndroid()
         {
+            if (Application.identifier.ToString().Contains("dev"))
+            {
+                PlayerSettings.productName = "testing_" + Application.version.ToString();
+            }
+            else
+            {
+                PlayerSettings.productName = "master_" + Application.version.ToString();
+            }
+
             var result = BuildPipeline.BuildPlayer(new BuildPlayerOptions
             {
                 
@@ -25,11 +34,9 @@ namespace UnityEditor
 
         public static string pathname
         {
-            
-
             get
             {
-                return (Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "builds"));
+                return "C:\\Users\\dadiu\\Dropbox\\DADIU_Team4\\Minigame1\\090_pipeline\\Apks\\";
             }
         }
 
@@ -38,7 +45,15 @@ namespace UnityEditor
 
             get
             {
-                return ("Build" + ".apk");
+                Debug.Log("here");
+                if (Application.identifier.ToString().Contains("dev"))
+                {
+                    return ("testing_build" + ".apk");
+                }
+                else
+                {
+                    return ("master_build" + ".apk");
+                }
             }
         }
 
