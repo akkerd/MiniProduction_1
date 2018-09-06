c8f6eef576f6a641ed893748f34a7a23f8cc0ec2
jonas billeskov <jonas.aksel@gmail.com>
2018-09-06 11:07:48 +0200

    First push
    Script for blendshapes
    Placeholder model
    Prefab

---
+++ MiniProduction_1/Assets/Scripts/BlendShapesScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendShapesScript : MonoBehaviour {
+
+    SkinnedMeshRenderer SMRenderer;
+    public int WeightValue = 50;
+
+	// Use this for initialization
+	void Start () {
+
+        SMRenderer = GetComponent<SkinnedMeshRenderer>();
+
+        SMRenderer.SetBlendShapeWeight(0, WeightValue);
+		
+	}
+	
+
+}
