59ab7d046cf15c4a8e0fe3806e9cb8dd96d5e5d8
Mikkel Kolding <mikkelkolding@yahoo.com>
2018-09-06 14:45:59 +0200

    Setup Wwise
    Wwise has been integrated in our project! Praise the Nintendo Gods!

---
+++ MiniProduction_1/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CLANKS = 752289768U;
+        static const AkUniqueID PLAY_FUCKBOISZ = 3701179968U;
+        static const AkUniqueID PLAY_VENTILATION = 2644260023U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID TEST = 3157003241U;
+        static const AkUniqueID VIRK_NU = 2760163333U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
