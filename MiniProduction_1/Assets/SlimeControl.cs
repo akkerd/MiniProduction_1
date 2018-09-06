3d324c113596f7e6e6eae5092926a0fe70dc20da
jonas billeskov <jonas.aksel@gmail.com>
2018-09-06 15:12:55 +0200

    slime

---
+++ MiniProduction_1/Assets/SlimeControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeControl : MonoBehaviour {
+
+    private void Update()
+    {
+        SlimeSpawn.SliderVal = transform.position.y;
+        Debug.Log(SlimeSpawn.SliderVal);
+
+    }
+}
---
+++ MiniProduction_1/Assets/SlimeSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawn : MonoBehaviour {
+
+    public static float SliderVal =20;
+    Cloth ClothComp;
+
+    private void Start()
+    {
+        ClothComp = gameObject.GetComponent<Cloth>();
+        ClothComp.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        
+        if (SliderVal < transform.position.y)
+        {
+            ClothComp.enabled = true;
+            if (ClothComp.externalAcceleration.x > 0)
+            {
+                ClothComp.externalAcceleration += new Vector3(-0.2f, 0, 0);
+            }
+        }
+		
+	}
+}
