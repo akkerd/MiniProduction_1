8dffa097e33acbaf93defba25fd358057f6b8c27
jonas billeskov <jonas.aksel@gmail.com>
2018-09-10 16:03:25 +0200

    first commit

--- MiniProduction_1/Assets/Scripts/ButtonUsleeve.cs
+++ MiniProduction_1/Assets/Scripts/ButtonUsleeve.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class ButtonUsleeve : MonoBehaviour {
 
 
-    private Material mat;
+  
 
 	// Use this for initialization
 	void Start () {
-        mat = GetComponent<Renderer>().material;
+      
 		
 	}
 	void OnTouchDown()
     {
-        mat.color = Color.red;
+       
     }
     void OnTouchUp()
     {
-        mat.color = Color.green;
+      
     }
     void OnTouchStay()
     {
-        mat.color = Color.yellow;
+      
         
     }
 
---
+++ MiniProduction_1/Assets/Scripts/TableCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableCollision : MonoBehaviour {
+
+    public GameObject Table;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject== Table)
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Invoke("CloseDown", 5);
+        }
+
+   
+    }
+
+    public void CloseDown()
+    {
+        GetComponent<SlimeSpawn>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        transform.parent = Table.transform;
+        Table.GetComponent<TableScript>().Moving = true;
+        GetComponent<TableCollision>().enabled = false;
+    }
+}
