4801e7f3318560f0b9027f534fcd21655793627e
Peter Spliid <pspliid@gmail.com>
2018-09-10 09:24:25 +0200

    Motion matching

---
+++ MiniProduction_1/Assets/Scripts/AnimTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour {
+
+    Animation anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+        //anim["clip"].time = 2;
+        anim.Play("walkCircleLeft_001_dt");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
