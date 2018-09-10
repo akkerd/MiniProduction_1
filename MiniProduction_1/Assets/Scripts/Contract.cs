06b9c5f43c8d355fda91644acc50fa567fce3d56
Simon Bøg <bla>
2018-09-10 11:07:31 +0200

    Setup of branch, ContractController

---
+++ MiniProduction_1/Assets/Scripts/Contract.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contract {
+
+	Stack[] stacks;
+
+	Contract(int numberOfStacks)
+	{
+		stacks = new Stack[numberOfStacks];
+	}
+}
