417a8dfa86cb322b1e7f81ea1e91b495a4fee365
Simon Bøg <bla>
2018-09-11 15:35:22 +0200

    Added stats to sleeves

--- MiniProduction_1/Assets/Scripts/Sleeve.cs
+++ MiniProduction_1/Assets/Scripts/Sleeve.cs
@@ -7,10 +7,57 @@ public class Sleeve {
     Color colorOfSleeve;
     public bool isEmpty;
 
+    //Behind the scenes stats
+    int strength;
+	int agility;
+	int intelligence;
+	int knowledge;
+	int beauty;
+	bool isFemale;
+
+    //Presentable Stats
+    float age;
+    float height;
+    float weight;
+    float bodyFatRatio;
+    float neuronCapacity;
+
     public Sleeve()
+    {
+
+    }
+    public Sleeve(bool makeEmpty)
     {
         isEmpty = true;
     }
+    public void SetStats(int newStrenght, int newAgility, int newIntelligence, int newKnowledge, int newBeauty, bool isMale)
+    {
+        strength = newStrenght;
+        agility = newAgility;
+        intelligence = newIntelligence;
+        knowledge = newKnowledge;
+        beauty = newBeauty;
+        isFemale = !isMale;
+    }
+    public void SetVisibleStats(float newAge, float newHeight, float newWeight,float newBodyFatRatio,float newNeuronCapacity)
+    {
+        age = newAge;
+        height = newHeight;
+        weight = newWeight;
+        bodyFatRatio = newBodyFatRatio;
+        neuronCapacity = newNeuronCapacity;
+    }
+    public void GetStats(out int[] stats, out bool isMale)
+    {
+        stats = new int[]{strength,agility,intelligence,knowledge,beauty};
+        isMale = !isFemale;
+    }
+    public void GetVisibleStats(out float[] visibleStats, out bool isMale)
+    {
+        visibleStats = new float[] {age,height,weight,bodyFatRatio,neuronCapacity};
+        isMale = !isFemale;
+    }
+    
     public Color ColorOfSleeve
     {
         get
