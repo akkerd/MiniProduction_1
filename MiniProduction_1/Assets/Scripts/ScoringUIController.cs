ff7037af81dfff2cc2322f429e06f1a692ee8af0
Simon Bøg <bla>
2018-09-11 09:09:07 +0200

    Scoring Screen made

---
+++ MiniProduction_1/Assets/Scripts/ScoringUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringUIController : MonoBehaviour {
+
+	[SerializeField]
+	GameObject scoringScreen;
+	[SerializeField]
+	Text[] stackCompletionFields;
+
+	public void ShowScoringScreen()
+	{
+		scoringScreen.SetActive(true);
+		Contract tempContract = ContractController.Instance.GetCurrentContract();
+		for (int i = 0; i < stackCompletionFields.Length; i++)
+		{
+			if (i < tempContract.GetStacks().Length)
+			{
+				stackCompletionFields[i].gameObject.SetActive(true);
+				//Implement measurement			
+			} else {
+				stackCompletionFields[i].gameObject.SetActive(false);
+			}
+		}
+	}
+	public void CloseScoringScreen()
+	{
+		scoringScreen.SetActive(false);
+	}	
+
+}
