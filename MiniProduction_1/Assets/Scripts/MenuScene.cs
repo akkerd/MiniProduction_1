8a01d369a9284d1bf9cecbc922fde521146cf431
Mikkel Kolding <mikkelkolding@yahoo.com>
2018-09-11 10:46:46 +0200

    Menu click sounds

--- MiniProduction_1/Assets/Scripts/MenuScene.cs
+++ MiniProduction_1/Assets/Scripts/MenuScene.cs
@@ -13,6 +13,7 @@ public class MenuScene : MonoBehaviour {
 	public void OnPlayClick()
     {
         Debug.Log("Pushed button");
+        AkSoundEngine.PostEvent("play_menu_select",gameObject);
         SceneManager.LoadScene(1);
         
     }
@@ -20,20 +21,24 @@ public class MenuScene : MonoBehaviour {
     public void OnQuitClick()
     {
         Debug.Log("Quit Game");
+        AkSoundEngine.PostEvent("play_menu_select",gameObject);
         Application.Quit();
     }
     public void OnSettingsClick()
     {
+        AkSoundEngine.PostEvent("play_menu_select", gameObject);
         settingsMenu.ChangeSettingsMenu();
         mainButtons.SetActive(false);
         backButton.SetActive(true);
     }
     public void OnCreditsClick()
     {
+        AkSoundEngine.PostEvent("play_menu_select", gameObject);
         credits.SetActive(true);
     }
     public void OnBackClick()
     {
+        AkSoundEngine.PostEvent("play_menu_select", gameObject);
         settingsMenu.ChangeSettingsMenu();
         backButton.SetActive(false);
         mainButtons.SetActive(true);
--- MiniProduction_1/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ MiniProduction_1/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,25 +13,34 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_CLICK = 311910498U;
         static const AkUniqueID PLAY_CLICKS = 2790046261U;
         static const AkUniqueID PLAY_ELECTRIC_ANOMALIES = 614939413U;
+        static const AkUniqueID PLAY_MENU_SELECT = 2167056030U;
         static const AkUniqueID PLAY_PLACEHOLDER_MUSIC = 3200335893U;
         static const AkUniqueID PLAY_SCREAMS = 2700195184U;
         static const AkUniqueID PLAY_VENTILATION = 2644260023U;
+        static const AkUniqueID PLAY_ZIPPER_LOOP = 3149298709U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSIC_MIX = 2883026413U;
+        static const AkUniqueID SFX_MIX = 681424821U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID AMBIENCE = 85412153U;
-        static const AkUniqueID CLICK = 1584507803U;
         static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
