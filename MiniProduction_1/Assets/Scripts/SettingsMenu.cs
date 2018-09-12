c1a4e3745b595f622e9c90c2b91286e17a375f05
jonas billeskov <jonas.aksel@gmail.com>
2018-09-12 11:08:02 +0200

    PlayerPrefSaving

--- MiniProduction_1/Assets/Scripts/SettingsMenu.cs
+++ MiniProduction_1/Assets/Scripts/SettingsMenu.cs
@@ -18,6 +18,18 @@ public class SettingsMenu : MonoBehaviour
     public Image english;
     public Image danish;
 
+
+    public void Start()
+    {
+        musicVolumeSlider.value = PlayerPrefs.GetInt("MusicVolume") ;
+        sfxVolumeSlider.value = PlayerPrefs.GetInt("SFXVolume");
+        musicToggle.isOn = (PlayerPrefs.GetInt("MusicToggle")!=1) ;
+        sfxToggle.isOn = (PlayerPrefs.GetInt("SFXToggle") != 1);
+        SettingsFlags.Instance.CurrentLanguage = (SettingsFlags.Language) PlayerPrefs.GetInt("FlagToggle"); ;
+
+    }
+
+
     public void ChangeSettingsMenu()
     {
         if (settingsVisible)
@@ -36,9 +48,11 @@ public class SettingsMenu : MonoBehaviour
 
     public void ChangeMusicValue()
     {
+        SettingsFlags.Instance.MusicVolume = (int)musicVolumeSlider.value;
+        PlayerPrefs.SetInt("MusicVolume", (int)musicVolumeSlider.value);
         if (SettingsFlags.Instance.IsMusicOn)
         {
-            SettingsFlags.Instance.MusicVolume = (int)musicVolumeSlider.value;
+           
             AkSoundEngine.SetRTPCValue("music_mix", musicVolumeSlider.value);
         }
     }
@@ -49,21 +63,26 @@ public class SettingsMenu : MonoBehaviour
 
         if (SettingsFlags.Instance.IsMusicOn)
         {
+            
             AkSoundEngine.SetRTPCValue("music_mix", SettingsFlags.Instance.MusicVolume);
             Debug.Log("Music muted");
+            PlayerPrefs.SetInt("MusicToggle",0);
         }
         else
         {
             AkSoundEngine.SetRTPCValue("music_mix", 0);
             Debug.Log("Music unmuted");
+            PlayerPrefs.SetInt("MusicToggle", 1);
         }
     }
 
     public void ChangeSFXValue()
     {
+        SettingsFlags.Instance.SFXVolume = (int)sfxVolumeSlider.value;
+        PlayerPrefs.SetInt("SFXVolume", (int)sfxVolumeSlider.value);
         if (SettingsFlags.Instance.IsSFXOn)
         {
-            SettingsFlags.Instance.SFXVolume = (int)sfxVolumeSlider.value;
+            
             AkSoundEngine.SetRTPCValue("sfx_mix", sfxVolumeSlider.value);
         }
 
@@ -77,11 +96,13 @@ public class SettingsMenu : MonoBehaviour
         {
             AkSoundEngine.SetRTPCValue("sfx_mix", SettingsFlags.Instance.SFXVolume);
             Debug.Log("SFX muted");
+            PlayerPrefs.SetInt("SFXToggle", 0);
         }
         else 
         {
             AkSoundEngine.SetRTPCValue("sfx_mix", 0);
             Debug.Log("SFX unmuted");
+            PlayerPrefs.SetInt("SFXToggle", 1);
         }
     }
 
@@ -97,10 +118,12 @@ public class SettingsMenu : MonoBehaviour
         {
             english.color = chosen;
             danish.color = unChosen;
+            PlayerPrefs.SetInt("FlagToggle", 0);
         } else
         {
             english.color = unChosen;
             danish.color = chosen;
+            PlayerPrefs.SetInt("FlagToggle", 1);
         }
     }
 }
