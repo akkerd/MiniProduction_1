f08ebb8737180eb4ea009fda8fe27e26fcda72a1
Peter Spliid <pspliid@gmail.com>
2018-09-13 17:19:42 +0200

    Motion matching DONE (kinda)

---
+++ MiniProduction_1/Assets/Scripts/MotionMatchingLoader.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Matching
+{
+    public int[][] to_idx_mapper;
+    public FrameMapper[] to_frame_mapper;
+    public int[][] neighbors;
+    public float[][] neighbors_dist;
+    public int[] animation_total_frames;
+}
+
+public class FrameMapper
+{
+    public string AniName;
+    public int AniIndex;
+    public int Frame;
+}
+
+public class MotionMatchingLoader : Manager<MotionMatchingLoader> {
+
+    public Matching StasisMatching;
+    public Matching ConfusedMatching;
+    public Matching ScaredMatching;
+    public Matching StrongMatching;
+    public Matching ToStrong;
+    public Matching ToConfused;
+    public Matching ToScared;
+    public int test;
+    // Use this for initialization
+    protected override void onAwake () {
+        StasisMatching = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\stasis_matching.json"));
+        ConfusedMatching = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\confused_matching.json"));
+        ScaredMatching = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\scared_matching.json"));
+        StrongMatching = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\strong_matching.json"));
+        ToStrong = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\to_strong_matching.json"));
+        ToConfused = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\to_confused_matching.json"));
+        ToScared = JsonConvert.DeserializeObject<Matching>(File.ReadAllText(@"Assets\Animations\Data\to_scared_matching.json"));
+    }
+
+    public Matching FromState(State state)
+    {
+        switch(state)
+        {
+            case State.Stasis:
+                return StasisMatching;
+            case State.Confused:
+                return ConfusedMatching;
+            case State.Scared:
+                return ScaredMatching;
+            case State.Strong:
+                return StrongMatching;
+        }
+        return null;
+    }
+}
